2264a40a50951dba6804ded736ce9190fe281561
Rana Sharjeel Ali <89970169+RanaSharjeelShji@users.noreply.github.com>
2023-11-10 23:03:45 +0500

    Update firebaseservices.dart

--- lib/firebaseservices.dart
+++ lib/firebaseservices.dart
@@ -1,25 +1,28 @@
-    import 'package:fbsocial/model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:loginwithfacebook/model.dart';
 
-Future<Resource?> signInWithFacebook() async {
-  var _auth;
-      try {
-      final LoginResult result = await FacebookAuth.instance.login();
-    switch (result.status) {
-      case LoginStatus.success:
-        final AuthCredential facebookCredential =
-            FacebookAuthProvider.credential(result.accessToken!.token);
-        final userCredential =await _auth.signInWithCredential(facebookCredential);
-        return Resource(status: Status.Success);
-      case LoginStatus.cancelled:
-        return Resource(status: Status.Cancelled);
-      case LoginStatus.failed:
-        return Resource(status: Status.Error);
-      default:
-        return null;
-      }
-      } on FirebaseAuthException catch (e) {
-      throw e;
+Future<UserCredential> signInWithFacebook() async {
+  try {
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    if (loginResult.status == LoginStatus.success) {
+      final AccessToken accessToken = loginResult.accessToken!;
+      final OAuthCredential credential = FacebookAuthProvider.credential(accessToken.token);
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } else {
+      throw FirebaseAuthException(
+        code: 'Facebook Login Failed',
+        message: 'The Facebook login was not successful.',
+      );
     }
+  } on FirebaseAuthException catch (e) {
+    // Handle Firebase authentication exceptions
+    print('Firebase Auth Exception: ${e.message}');
+    throw e; // rethrow the exception
+  } catch (e) {
+    // Handle other exceptions
+    print('Other Exception: $e');
+    throw e; // rethrow the exception
   }
+}
